abba98bc916fa74c13a6a8393a6139397099acb7
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-02-23 11:20:45 +0100

    always find background-border.jpg

--- scripts/merge_pdf_with_background.pl
+++ scripts/merge_pdf_with_background.pl
@@ -3,6 +3,8 @@ use strict;
 
 use PDF::API2;
 
+use File::Basename;
+
 use constant mm => 25.4 / 72;
 use constant in => 1 / 72;
 use constant pt => 1;
@@ -17,7 +19,7 @@ my $pdf_out = new PDF::API2 (-file => $pdf_out_name) or die;
 
 my $pdf_in  = PDF::API2->open ($pdf_in_name) or die;
 
-my $background = $pdf_out -> image_jpeg('..\out\background-border.jpg');
+my $background = $pdf_out -> image_jpeg(dirname(__FILE__) . '/../out/background-border.jpg');
 
 for my $page_no (1 .. $pdf_in -> pages()) {
 
